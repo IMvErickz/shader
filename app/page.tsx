1152dcf9e4f0f01beda6f529c0ca2c343c7d6ffb
IMvErickz <erickspy2003@gmail.com>
2024-07-18 08:19:24 -0300

    fix: paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-800">
       <h1 className="text-white text-6xl font-semibold">Shader</h1>
-      <p className="text-white font-semibold">Coming soon...</p>
+      <p className="text-white font-semibold">Comming soon...</p>
     </main>
   );
 }
