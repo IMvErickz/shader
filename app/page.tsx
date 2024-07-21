1c6a5588790cb3c4232a48b18fe754471cd17885
IMvErickz <erickspy2003@gmail.com>
2024-07-21 19:07:27 -0300

    feat: add react-chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { LineChart } from "@/components/dashboard/line-chart";
+
 export default function Home() {
+
   return (
-    <main className="flex flex-col items-center justify-between p-24 bg-zinc-800">
-      <img src="/favicon.ico" alt="" />
-      <h1 className="text-orange-600 text-6xl font-semibold">Shader</h1>
-      <p className="text-white font-semibold">Comming soon...</p>
+    <main className="size-full flex flex-col items-center p-24 bg-zinc-800">
+      <section className="size-full flex flex-col items-center">
+        <div className="w-full flex items-center">
+          <h1 className="text-white text-3xl font-semibold">Produtos mais vendidos</h1>
+        </div>
+        <div className="size-full flex items-center justify-center">
+          <LineChart />
+        </div>
+      </section>
     </main>
   );
 }
---
+++ components/dashboard/line-chart.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement, LineElement
+);
+import { Line } from 'react-chartjs-2';
+
+export function LineChart() {
+    const labels = ['1', '2', '3', '4', '5', '6', '7']
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset vermelho',
+                data:
+                    labels.map((() => Math.floor(Math.random() * 100))),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderColor: 'red',
+                borderWidth: 1
+            },
+            {
+                label: 'Dataset azul',
+                data:
+                    labels.map((() => Math.floor(Math.random() * 100))),
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                borderColor: 'blue',
+                borderWidth: 1
+            },
+        ],
+    }
+
+    return (
+        <Line options={{ responsive: true }} data={data} />
+    )
+}
