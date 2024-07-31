a4b11a52d01a109129f3cd2582d52b7eccc66405
IMvErickz <erickspy2003@gmail.com>
2024-07-30 21:35:08 -0300

    feat: add pie chart and redirect login without cookie

--- components/layout/conditional-layout.tsx
+++ components/layout/conditional-layout.tsx
@@ -3,9 +3,18 @@
 import { usePathname } from 'next/navigation';
 import { SignInHeader } from '../sign-in/header';
 import { Header } from '../header';
+import { parseCookies } from 'nookies';
+import { useRouter } from 'next/navigation';
 
 export const ConditionalLayout = () => {
     const pathname = usePathname();
+    const router = useRouter()
+
+    const cookies = parseCookies()
+
+    if (!cookies['@token']) {
+        router.replace('/sign-in')
+    }
     return (
         <>
             {(pathname === "/sign-in" ? <SignInHeader /> : (<Header />))}
