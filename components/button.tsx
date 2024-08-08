7682e399064d730bb62ed21f1835789d84bc1f63
IMvErickz <erickspy2003@gmail.com>
2024-08-07 21:32:20 -0300

    feat: add product card

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,16 @@
 import { ComponentProps, ReactNode } from "react";
+import { twMerge, twJoin } from "tailwind-merge";
 
 interface ButtonProps extends ComponentProps<'button'> {
     children: ReactNode
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, className, ...props }: ButtonProps) {
+    const mergedClassName = twJoin('w-full h-12 flex items-center justify-center bg-orange-600 rounded text-white font-semibold hover:bg-orange-700 transition-colors disabled:cursor-not-allowed disabled:bg-zinc-400', className)
+
     return (
         <button
-            className="w-full h-12 flex items-center justify-center bg-orange-600 rounded text-white font-semibold hover:bg-orange-700 transition-colors disabled:cursor-not-allowed disabled:bg-zinc-400"
+            className={mergedClassName}
             {...props}>
             {children}
         </button>
