21b060b618dd3251fd4abaa5e7d1ade642acbd93
IMvErickz <erickspy2003@gmail.com>
2024-08-07 22:54:49 -0300

    feat: add product modal register

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,13 @@
 import { Logo } from "@/public/icons/Logo";
 import { NavLink } from "./nav-link";
+import { usePathname } from "next/navigation";
+import { RegisterModal } from "./Products/RegisterModal";
 
 export function Header() {
+    const path = usePathname()
+
     return (
-        <header className="w-full h-20 border-b-2 flex items-center border-b-zinc-500 border-b-solid px-4 gap-x-4">
+        <header className="w-full h-20 border-b-2 flex items-center justify-between border-b-zinc-500 border-b-solid px-4 gap-x-4">
             <Logo />
             <nav className="w-full h-full items-center flex gap-x-8">
                 <NavLink href="/">
@@ -14,6 +18,11 @@ export function Header() {
                     Products
                 </NavLink>
             </nav>
+            <div className="w-56">
+                {path === '/products' && (
+                    <RegisterModal />
+                )}
+            </div>
         </header>
     )
 }
