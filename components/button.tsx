e151fd97b202b03c67bb71d99dfdd445b30bf2be
IMvErickz <erickspy2003@gmail.com>
2024-08-09 09:00:44 -0300

    fix: object image: remove button in product card

--- components/button.tsx
+++ components/button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps extends ComponentProps<'button'> {
 }
 
 export function Button({ children, className, ...props }: ButtonProps) {
-    const mergedClassName = twJoin('w-full h-12 flex items-center justify-center bg-orange-600 rounded text-white font-semibold hover:bg-orange-700 transition-colors disabled:cursor-not-allowed disabled:bg-zinc-400', className)
+    const mergedClassName = twMerge('w-full h-12 flex items-center justify-center bg-orange-600 rounded text-white font-semibold hover:bg-orange-700 transition-colors disabled:cursor-not-allowed disabled:bg-zinc-400', className)
 
     return (
         <button
