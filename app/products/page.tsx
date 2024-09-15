42ca1066e18d08f4166b40a8e301c5c4fecfc5f7
IMvErickz <erickspy2003@gmail.com>
2024-09-15 17:22:26 -0300

    fix: add suspense to work the useSearchParams hook

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,19 @@
 import { ShowProductSection } from '@/components/Products/show-product-section'
 import { ToogleView } from '@/components/toogle-view'
+import { Suspense } from 'react'
 
 export default function ProductsPage() {
   return (
     <main className="size-full flex flex-col items-center justify-center">
       <section className="w-full flex items-center justify-between px-8 py-4">
         <h1 className="text-white text-4xl font-bold">Produtos Cadastrados</h1>
-        <ToogleView />
+        <Suspense>
+          <ToogleView />
+        </Suspense>
       </section>
-      <ShowProductSection />
+      <Suspense>
+        <ShowProductSection />
+      </Suspense>
     </main>
   )
 }
