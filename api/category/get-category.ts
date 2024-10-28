1265d7868ad908cb61e6b49fc7f677aa3be21269
IMvErickz <erickspy2003@gmail.com>
2024-10-28 19:27:24 -0300

    fix: send token in category request

--- api/category/get-category.ts
+++ api/category/get-category.ts
@@ -1,8 +1,15 @@
 import { Category } from '@/@types/category/category'
 import { api } from '@/lib/axios'
+import { getToken } from '../get-token'
 
 export async function getCategory() {
-  const response = await api.get<Category[]>('/category')
+  const token = getToken()
+
+  const response = await api.get<Category[]>('/category', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
 
   return response.data
 }
